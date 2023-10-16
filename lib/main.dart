149603a3deca510e56c5061416e9b17ba1874872
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-10-16 20:24:58 +0900

    appbar delete

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,6 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello flutter!'),
-        ),
         body: Center(
           child: Text('Hello world!'),
         ),
