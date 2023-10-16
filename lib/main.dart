f53103e3f6d50f2692f21137d441e1a2e1e18b66
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-10-16 20:48:32 +0900

    Row mainAxisAlignment end

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class App extends StatelessWidget {
                 height: 80,
               ),
               Row(
+                mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   Column(
                     children: [
